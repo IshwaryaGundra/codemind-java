f7abbb64634fa3538baca51c726dc93a0293b294
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-08-27 19:39:05 +0530

    import java.util.Scanner; class aschi{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         //char ch=sc.next().charat(0);         char c = sc.next().charAt(0);         int aschi=c;         System.out.println(aschi);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class aschi{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        //char ch=sc.next().charat(0);
+        char c = sc.next().charAt(0);   
+        int aschi=c;
+        System.out.println(aschi);
+    }
+}
