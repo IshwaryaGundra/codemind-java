32cc4e3c44fae1ee2a6f96b37e87f75098585ef4
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-08-27 19:28:30 +0530

    import java.util.Scanner; import java.lang.Math; class sum{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         float a=sc.nextFloat();         float b=sc.nextFloat();
            System.out.println(""+String.format("%.2f",a*b));
        }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.lang.Math;
+class sum{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        float b=sc.nextFloat();
+        
+        System.out.println(""+String.format("%.2f",a*b));
+    }
+}
