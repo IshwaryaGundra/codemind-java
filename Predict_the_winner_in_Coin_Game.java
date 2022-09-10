feeb8796b7af7303ae173683b25ef02eb9cbad36
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-09-10 13:50:41 +0530

    import java.util.Scanner; class Gam{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int m=sc.nextInt();         int n=sc.nextInt();         if(m%2==0 || n%2==0)         {             System.out.println("Player 1");         }         else         {              System.out.println("Player 2");         }     }
    }

---
+++ Predict_the_winner_in_Coin_Game.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Gam{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        if(m%2==0 || n%2==0)
+        {
+            System.out.println("Player 1");
+        }
+        else
+        {
+             System.out.println("Player 2");
+        }
+    }
+        
+}
