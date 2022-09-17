0f2cb0f4e6fe1f31981e4ab862a2f344b3053c6d
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-09-17 19:35:33 +0530

    import java.util.Scanner; class Avg{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n= sc.nextInt();         int s=0,c=0;         int arr[]=new int[100];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         int m=arr[0];         for(int i=0;i<n;i++){             if(m>arr[i]){                  m=arr[i];             }         }         System.out.println(m); } }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Avg{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int s=0,c=0;
+        int arr[]=new int[100];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int m=arr[0];
+        for(int i=0;i<n;i++){
+            if(m>arr[i]){
+                 m=arr[i];
+            }
+        }
+        System.out.println(m);
+}
+}
