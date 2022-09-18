3ae535cafee7211ea609d12be9b375049f57ab58
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-09-18 11:13:04 +0530

    import java.util.Scanner; class Tab{     public static void main(String args[]){          Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int r=sc.nextInt();         for(int i=1;i<=r;i+=2){             System.out.println(n +" x "+ i +" = " +n*i);         }     } }

---
+++ Tables.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Tab{
+    public static void main(String args[]){
+         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int r=sc.nextInt();
+        for(int i=1;i<=r;i+=2){
+            System.out.println(n +" x "+ i +" = " +n*i);
+        }
+    }
+}
